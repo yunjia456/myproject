2b1a4f544f2abda755570f9b5d23841de4b0b9fb
Wilson <wilson555456@gmail.com>
2021-08-01 13:59:56 +0800

    Initial commit

---
+++ src/main/java/com/wilson/Hello.java
@@ -0,0 +1,10 @@
+package com.wilson;
+
+public class Hello {
+    public static void main(String[] args) {
+       // System.out.println("Hello wilson!");
+       // new Person().hello();
+        Person p = new Person();
+        p.hello();
+    }
+}
---
+++ src/main/java/com/wilson/Person.java
@@ -0,0 +1,7 @@
+package com.wilson;
+
+public class Person {
+    public void hello() {
+        System.out.println("Hello Wilson Person");
+    }
+}
